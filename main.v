a738fe2d04d8620f8359306cf80697a8bb4ab211
ZaViBiS <igordnepr27@gmail.com>
2021-11-13 21:49:47 +0200

    переписал [get_file_size]

--- main.v
+++ main.v
@@ -82,33 +82,12 @@ fn main() {
 
 fn get_file_size(data http.Response) int {
 	// получает размер файла в bytes по url'у
-	if http.status_from_int(data.status_code).is_error() {
-		println('failed to get data from server with status code $data.status_code')
+	if http.status_from_int(data.status_code).is_success() {
+		return data.header.values(http.CommonHeader.content_length)[0].int()
+	}else {
+		println('failed to get data from server with status code [$data.status_code]')
 		exit(1)
 	}
-	mut result := ''
-	mut n := ''
-	mut b := false
-	for x in data.header.str().runes() {
-		if b {
-			if x.str() != '\n' {
-				result += x.str()
-			}else {
-				return result.int()
-			}
-		}
-		if n == 'Content-Length:' {
-			n = ''
-			b = true
-		}
-		if x.str() == '\n' {
-			n = ''
-		}else {
-			n += x.str()
-		}
-	}
-	println('something went wrong when receiving data from the server')
-	exit(1)
 }
 
 fn resp(url string, interval string) []byte {
